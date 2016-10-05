bcb8150ac70c8197d5c5e4d4a337f5eee4a92fda
Chandler Kelley <chandler.t.kelley@gmail.com>
2016-10-04 22:05:43 -0400

    Can send notes to the client

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :notes
-  resources :recipes
+  resources :recipes do
+  	resources :notes
+  end
   # get 'recipes/index_page/:id', to 'recipes#index_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
